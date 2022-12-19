bb15afc7960f76bbbddda22f3718071e061b64bb
drfifonz <fp.patyk@gmail.com>
2022-12-19 02:43:53 +0100

    feat: add refactoring bbox cordinates & image's file name in list_bbox.txt

--- src/datasets/preprocess.py
+++ src/datasets/preprocess.py
@@ -20,10 +20,11 @@ class Preprocess:
     class for preprocessing preperation images for faster loading
     """
 
-    def __init__(self, dataset_path) -> None:
+    def __init__(self, dataset_path: str, scale: float) -> None:
         self.dataset_path = dataset_path
         self.utils = LandmarkUtils(self.dataset_path)
         self.img_to_tensor = transforms.ToTensor()
+        self.scale = scale
 
     def save_resized_imgs(self, image_scale: float) -> None:
         """
@@ -66,6 +67,52 @@ class Preprocess:
 
             torch.save(img_tensor, img_tensor_path)
 
+    def refactor_landmarks_file(self, landmarks_file_path, as_tensors: bool = False) -> None:
+        """
+        Refactor list_landmarks.txt file by changing image's file name
+        """
+        # TODO implement refactor_landmarks_method
+        pass
+
+    def refactor_bbox_file(self, bbox_file_path: str, as_tensors: bool = False) -> None:
+        """
+        Refactor list_bbox.txt file by changing image's file name and calculating bbox by scale
+        """
+
+        file_root_path, full_file_name = os.path.split(bbox_file_path)
+        file_name, ext = os.path.splitext(full_file_name)
+
+        file_name += f"_{self.scale}"
+        if as_tensors:
+            file_name += "_tensors"
+        print(file_name)
+        list_lines = self.__open_txt_file(bbox_file_path).split("\n")
+        for index, line in enumerate(list_lines[2:]):
+
+            line = line.replace("img/", f"img_{self.scale}/")
+            if as_tensors:
+
+                line = line.replace(".jpg", ".pt")
+
+            list_line = line.split(" ")
+
+            for line_index, coordinate in enumerate(list_line[1:]):
+                list_line[line_index + 1] = f"{int(int(coordinate) * self.scale):03d}"
+            line = " ".join(list_line)
+            list_lines[2 + index] = line
+
+        text = "\n".join(list_lines)
+
+        self.__save_txt_file(os.path.join(file_root_path, file_name + ext), text)
+
+    def __open_txt_file(self, path: str) -> str:
+        with open(path, "r", encoding="utf-8") as file:
+            return file.read()
+
+    def __save_txt_file(self, path: str, text: str) -> str:
+        with open(path, "w", encoding="utf-8") as file:
+            return file.write(text)
+
     def __create_dir(self, path: str) -> None:
         if not os.path.exists(path):
             os.mkdir(path)
@@ -75,10 +122,13 @@ if __name__ == "__main__":
 
     SCALE = 0.5
 
+    BBOX_PATH = "data/datasets/Deepfashion_Landmark/Anno/list_bbox.txt"
+
     lm_dataset_path = os.path.join(cfg.LANDMARK_DATASET_PATH, cfg.LM_IMGS_DIR_PATH)
-    preprocess = Preprocess(lm_dataset_path)
+    preprocess = Preprocess(lm_dataset_path, SCALE)
 
     # preprocess.save_resized_imgs(SCALE)
 
-    preprocess.save_imgs_as_tensors("img_0.5")
+    preprocess.refactor_bbox_file(BBOX_PATH, True)
+    # preprocess.save_imgs_as_tensors("img_0.5")
     # preprocess.save_imgs_as_tensors()
