8785807855c8ef4f2883a6a01da50e7841cd965f
drfifonz <fp.patyk@gmail.com>
2023-01-26 01:07:25 +0100

    feat(server): add testing connection by returning N random images, N is value got in request
    fix conflits after bad merge in server.py

--- src/server/server.py
+++ src/server/server.py
@@ -1,33 +1,34 @@
-import base64
-import io
 import json
-import sys,os
+import os
+import sys
+
 from flask import Flask, request
-from PIL import Image
+
 sys.path.append("./src")
-from utils.server_utils import ServerUtil
+
+# pylint: disable=wrong-import-position
+from utils.server_utils import ServerUtils
+# pylint: enable=wrong-import-position
+
+
 
 IMAGE_KEY = "photo"
-RELATIVE_PATH = "data/image.jpg"
+NUM_RES_KEY = "numResults"
+
 
 app = Flask(__name__)
 
+server_util = ServerUtils()
 
 @app.route("/", methods=["GET"])
 def webhook():
     """
     basic webhoor for GET requesst
     """
-    """
-    basic webhoor for GET requesst
-    """
     return "200"
 
 @app.route("/", methods=["POST"])
-def receive_post_image():
-    """
-    receive by POST request image in json format
-    """
+
 
 def receive_post_image():
     """
@@ -38,33 +39,48 @@ def receive_post_image():
 
         payload = request.json
         img_string = payload[IMAGE_KEY]
-        img_bytes = base64.b64decode(img_string)
-        img_pil = Image.open(io.BytesIO(img_bytes))
-        img_pil.show()  # showing sended photo
-        # response_messages = {"response_message": "OK"}
-        server_util = ServerUtil()
-        response_messages = server_util.get_json_response(RELATIVE_PATH)
+        num_results = payload[NUM_RES_KEY]
+
+
+        #FOR TESTING PURPOUSE
+        image_from_req = server_util.image_base64_to_pil(img_string)
+        image_from_req.show(title="Image from request")
+
+        pil_response_images = server_util.get_random_pil_images(num_results)
+
+        #FOR TESTING PURPOUSE
+        server_util.combine_images(pil_response_images,show_image=True) # shows randomly selected images
+
+
+
+        bytes_response_images = [server_util.image_pil_to_base64(image) for image in pil_response_images]
+
+        list_of_nested_dics =[{"image":image} for image in bytes_response_images]
+        response_message = {
+                            "hello":"world",
+                            "images": list_of_nested_dics
+                        }
+        print(type(num_results),num_results)
+
+
+
 
         response_status = 200
-    except TypeError:
     except TypeError:
         if request.is_json:
             payload = request.json
             try:
                 img_value = payload[IMAGE_KEY]
             except TypeError:
-                img_value = payload[IMAGE_KEY]
-            except TypeError:
-                img_value  = None
-
-        response_messages = {"is_json": request.is_json,
+                img_value = None
+        response_message = {"is_json": request.is_json,
                              "img_value_type": str(type(img_value)),
                              "img_value": str(img_value)}
-        response_status = 400
+
         response_status = 400
 
     return app.response_class(
-        response=json.dumps(response_messages), status=response_status, mimetype="application/json")
+        response=json.dumps(response_message), status=response_status, mimetype="application/json")
 
 
 if __name__ == "__main__":
--- src/utils/server_utils.py
+++ src/utils/server_utils.py
@@ -1,13 +1,19 @@
 import base64
 import json
 import io
+import os
+import random
 from PIL import Image
+import numpy as np
+
+import utils.config as cfg
 
 
-class ServerUtil():
+class ServerUtils:
     """
-    Helper class for creating JSON responses
+    Helper class for server
     """
+
     def __init__(self) -> None:
         pass
 
@@ -16,17 +22,24 @@ class ServerUtil():
         Converts given image path to base64
         """
         with open(image_str, "rb") as image:
-            return base64.b64encode(image.read()).decode('utf-8')
+            return base64.b64encode(image.read()).decode("utf-8")
 
-    def __image_pil_to_base64(self, image: Image) -> str:
+    def image_pil_to_base64(self, image: Image.Image) -> str:
         """
-        Converts a PIL Image to base64
+        Converts a PIL Image to base64 string
         """
         buffer = io.BytesIO()
-        image.save(buffer, format = image.format)
+        image.save(buffer, format=image.format)
         buffer = buffer.getvalue()
-        return base64.b64encode(buffer).decode('utf-8')
+        return base64.b64encode(buffer).decode("utf-8")
 
+    def image_base64_to_pil(self, img_string: str) -> Image.Image:
+        """
+        convert string in base64 format to PIL image
+        """
+        img_bytes = base64.b64decode(img_string)
+        img_pil = Image.open(io.BytesIO(img_bytes))
+        return img_pil
 
     def get_json_response(self, image_path: str) -> str:
         """
@@ -34,21 +47,46 @@ class ServerUtil():
         """
         encoded = self.__image_path_to_base64(image_path)
 
-        message = {
-            "returned_image": encoded
-        }
+        message = {"returned_image": encoded}
         return json.dumps(message)
 
-    def get_json_response_pil(self, image: Image) -> str:
+    def get_json_response_from_pil(self, image: Image) -> str:
         """
         Returns JSON message with base64 encoded image from PIL Image
         """
-        encoded = self.__image_pil_to_base64(image)
+        encoded = self.image_pil_to_base64(image)
 
-        message = {
-            "returned_image": encoded
-        }
+        message = {"returned_image": encoded}
         return json.dumps(message)
 
-#ser = ServerUtil()
-#print(ser.get_json_response("data/image.jpg"))
+    def combine_images(self, images: list[Image.Image], show_image: bool = False) -> Image.Image:
+        """
+        (for local tests only) stack images to one horizontaly
+        """
+        resized_images_list = list(map(lambda photo: photo.resize((300, 500)), images))
+        np_images_list = [np.array(image) for image in resized_images_list]
+        stacked_image = np.hstack(np_images_list)
+        image = Image.fromarray(stacked_image)
+        if show_image:
+            image.show()
+
+        return image
+
+    def __get_random_images_paths(self, num_of_images: int) -> list[str]:
+        """
+        return paths to random images from landmark dataset img dir
+        """
+        dir_path = os.path.join(cfg.LANDMARK_DATASET_PATH, cfg.LM_IMGS_DIR_PATH)
+        images = random.choices(os.listdir(dir_path), k=num_of_images)
+        images_paths = [os.path.join(dir_path, im) for im in images]
+
+        return images_paths
+
+    def get_random_pil_images(self, num_of_images: int) -> list[Image.Image]:
+
+        """
+        return list of random PIL images  from dataset
+        """
+        paths = self.__get_random_images_paths(num_of_images)
+
+        return [Image.open(path) for path in paths]
